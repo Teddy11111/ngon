eaf458fb369303a258c8491953c3441d20946c92
Ted Hoang <hthe.1997@gmail.com>
2021-01-29 10:46:05 +0300

    главная страница почти готова

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {ReactComponent as Logo} from './Logo.svg';
+import {ReactComponent as Logo2} from './Logo.svg';
 import {ReactComponent as Oxx} from './Oxx.svg';
 import {ReactComponent as LeftB} from './Left.svg';
 import {ReactComponent as RightB} from './Right.svg';
@@ -15,15 +15,15 @@ function App(){
       <span onClick>Меню</span>
       <span onClick style={{left: "580px"}}>Галерея</span>
       <span onClick style={{left: "710px"}}>Банкеты</span>
-      <span onClick style={{left: "840px"}}>Акции</span>
+      <span onClick style={{left: "840px"}}>Акции</span>  
       <span onClick style={{left: "970px"}}>Контакты</span>
     </div>
     <rect className="square"></rect>
-    <div className="text">Yeah!</div>
-    <img clasName="pho" src={Pho} alt="Pho"></img>
+    <div className="text">Скидка на ФоБо!</div>
+    <img className="pho" src={Pho} alt="Pho"></img>
     <div className="left"> <LeftB/> </div>
     <div className="right"> <RightB/> </div>
-    <div className="logo"> <Logo/> </div>
+    <div className="logo"> <Logo2/> </div>
     <div className="oxx"> <Oxx/> </div>
   </div>
   )
